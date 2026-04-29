72e2198c35781b6f3bbfda6aba39b1087a25f28f
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-04-29 23:29:18 +0600

    first commit with Dummy Navbar

---
+++ src/app/(main)/home/page.jsx
@@ -0,0 +1,12 @@
+
+
+const HomePage = () => {
+  return (
+    <div>
+      
+      My Home Page
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/(main)/page.js
@@ -0,0 +1,8 @@
+import { redirect } from "next/dist/server/api-utils";
+
+
+export default function Home() {
+  return (
+   redirect("/home")
+  );
+}
