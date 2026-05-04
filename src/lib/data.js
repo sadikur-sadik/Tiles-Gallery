ee7c4aa0fffc0afa0a3f0138ad2502cae111b502
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-05-04 11:25:11 +0600

    Cache in Use

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,5 @@
 export const AllTilesData = async () =>{
 
-  const res = await fetch('https://tiles-json-server.onrender.com/data')
+  const res = await fetch('https://tiles-json-server.onrender.com/data',{cache: 'force-cache'})
   return  res.json();
 }
