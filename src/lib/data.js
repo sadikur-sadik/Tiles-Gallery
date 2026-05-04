c6bc276d96c3aacdd91e30818c33b36c013b4e7e
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-05-04 15:31:37 +0600

    Caching method added

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,6 +1,6 @@
 export const AllTilesData = async () =>{
 
-  const res = await fetch('https://tiles-json-server.onrender.com/data');
+  const res = await fetch('https://tiles-json-server.onrender.com/data',{cache:'force-cache'});
 
   return  res.json();
 }
