b12217832100e3b02296180b857780a749bd487c
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-05-04 15:29:55 +0600

    Merge branch 'main' of https://github.com/sadikur-sadik/Tiles-Gallery

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,4 @@
 export const AllTilesData = async () =>{
-
-  const res = await fetch('https://tiles-json-server.onrender.com/data',{cache:'force-cache'});
+  const res = await fetch('https://tiles-json-server.onrender.com/data');
   return  res.json();
 }
